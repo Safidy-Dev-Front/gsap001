9c901795787b86d8cd3396773c1f748bf63526a1
Safidi <safidiniainaromino13@gmail.com>
2022-03-21 17:06:00 +0300

    animation fade and form Basic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+gsap.from('.navigation', { opacity: 0, y: '-100%', duration: 1, ease: 'Bounce.easeOut' });
+
+gsap.registerEffect({
+    name: "fade",
+    defaults: { duration: 2 }, //defaults get applied to the "config" object passed to the effect below
+    effect: (targets, config) => {
+        return gsap.to(targets, { duration: config.duration, opacity: 0 });
+    }
+});
+
+//now we can use it like this:
+//gsap.effects.fade(".box");
+
+document.querySelectorAll(".box").forEach(function(box) {
+    box.addEventListener("mouseenter", function() {
+        gsap.effects.fade(this);
+    });
+});
